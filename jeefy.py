e2c5d4d9c967432da8abb90adaea341f608f05a9
jeefies <jeefy163@163.com>
2021-01-02 02:57:09 +0800

    add migrate?

--- jeefy.py
+++ jeefy.py
@@ -1,7 +1,7 @@
 import os
 from app import create_app, db
 from app.models import File
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 
 app = create_app(os.getenv("FLASK_CONFIG", 'default'))
 migrate = Migrate(app, db)
@@ -9,6 +9,10 @@ migrate = Migrate(app, db)
 @app.shell_context_processor
 def make():
     return dict(db = db, File=File)
+
+@app.cli.command()
+def deploy():
+    upgrade()
 
 if __name__ == '__main__':
         try:
