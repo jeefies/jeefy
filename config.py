8ced2e41a2d6cc8adea9c43984e6aa8c7bc06024
jeefies <jeefy163@163.com>
2021-01-02 03:32:27 +0800

    config dburi?

--- config.py
+++ config.py
@@ -16,12 +16,13 @@ class Debug(Config):
     DEBUG = True
 
 class Heroku(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///" + os.path.join(base, 'data.sqlite'))
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgres://nqtbkqvkpfkabx:eb5a1a664d70e6be1be91414211a7b28b90d4d7145c3c07e7c15e701f2579999@ec2-34-197-212-240.compute-1.amazonaws.com:5432/d8el63dec4iqjb")
     SSL_REDIRECT = True if os.getenv('DYNO') else False
     PROXYFIX_USE = True if os.getenv('PROXYFIX_USE') else False
 
     @classmethod
     def init_app(cls, app):
+        print('database url', cls.SQLALCHEMY_DATABASE_URI)
         if cls.PROXYFIX_USE:
             print('proxyfix use')
             from werkzeug.middleware.proxy_fix import ProxyFix
